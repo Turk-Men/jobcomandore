37b7d4320c86fd0ec52391d6f745f8b4bb1e2485
Turk-Men <buruchenko84@gmail.com>
2022-05-22 22:34:27 +0700

    comandor
    comandor

---
+++ resources/lang/ru/auth.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    
+    'failed' => 'Эти учетные данные не соответствуют нашим записям.',
+    'throttle' => 'TСлишком много попыток входа. Повторите попытку через :seconds секунд.',
+    
+    'login' => 'Войти',
+    'register' => 'Зарегистрироваться',
+    'account_info' => 'Информация об учетной записи',
+    'change_info' => 'Изменить информацию',
+    'change_password' => 'Сменить пароль',
+    'logout' => 'Выйти',
+    'remember_me' => 'Запомни меня',
+    'information_updated' => 'Ваша информация обновлена.',
+    'name' => 'Имя',
+    'email' => 'Email',
+    'password' => 'Пароль',
+    'confirm_password' => 'Подтвердить Пароль',
+    'old_password' => 'Старый пароль',
+    'new_password' => 'Новый пароль',
+    'cancel' => 'Отмена',
+    'reset_password' => 'Сброс пароля',
+    'send_password_reset_link' => 'Отправить ссылку для сброса пароля',
+    'forgot_password_question' => 'Забыли Ваш пароль?',
+    'wrong_password' => 'Старый пароль, который вы ввели, неверен.',
+    'password_updated' => 'Ваш пароль был изменен.',
+    'registration_closed' => 'Регистрация закрыта.',
+
+];
